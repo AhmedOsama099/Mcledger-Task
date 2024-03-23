e2fcfbcee37b3222f4dede2250032b473b6a25e7
AhmedOsama099 <ahmedeng099@gmail.com>
2024-03-23 03:01:16 +0200

    add ui-server file with needed apis functions

---
+++ wizard-app/src/server/ui-server.tsx
@@ -0,0 +1,27 @@
+import { http, HttpResponse } from "msw";
+import { setupWorker } from "msw/browser";
+import * as singersDB from "./backend-apis/singers-apis";
+import * as albumsDB from "./backend-apis/albums-apis";
+import * as songsDB from "./backend-apis/songs-apis";
+
+const apiUrl = "https:tunes";
+
+export const worker = setupWorker(
+  // Get all singers
+  http.get(`${apiUrl}/singers/all`, async () => {
+    const singers = await singersDB.readAllSingers();
+    return HttpResponse.json(singers);
+  }),
+  // Get albums by singer ids
+  http.get(`${apiUrl}/album/:id`, async ({ params }) => {
+    const { id } = params;
+    const albums = await albumsDB.readAlbumsBySingersIds(id as string[]);
+    return HttpResponse.json(albums);
+  }),
+  // Get songs by album ids
+  http.get(`${apiUrl}/song/:id`, async ({ params }) => {
+    const { id } = params;
+    const songs = await songsDB.readSongsByAlbumsIds(id as string[]);
+    return HttpResponse.json(songs);
+  })
+);
