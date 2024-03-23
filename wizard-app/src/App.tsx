8fb53b3c7e54901fb727e7818d11bd9df6004d22
AhmedOsama099 <ahmedeng099@gmail.com>
2024-03-23 05:53:13 +0200

    add home, market, about, navbar, footer initial pages and create layout structure

--- wizard-app/src/App.tsx
+++ wizard-app/src/App.tsx
@@ -1,6 +1,28 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
 import "./globalStyles";
+import Home from "./pages/Home";
+import Market from "./pages/Market";
+import About from "./pages/About";
+import Layout from "./pages/Layout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="market" element={<Market />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
+
 function App() {
-  return <>Hello</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
