ada5032576778941a63909941d46beed5940ddc2
AhmedOsama099 <ahmedeng099@gmail.com>
2024-03-23 03:09:33 +0200

    install msw inside project and edit server func returns to return objects

--- wizard-app/src/server/ui-server.tsx
+++ wizard-app/src/server/ui-server.tsx
@@ -10,18 +10,18 @@ export const worker = setupWorker(
   // Get all singers
   http.get(`${apiUrl}/singers/all`, async () => {
     const singers = await singersDB.readAllSingers();
-    return HttpResponse.json(singers);
+    return HttpResponse.json({ singers: singers });
   }),
   // Get albums by singer ids
   http.get(`${apiUrl}/album/:id`, async ({ params }) => {
     const { id } = params;
     const albums = await albumsDB.readAlbumsBySingersIds(id as string[]);
-    return HttpResponse.json(albums);
+    return HttpResponse.json({ albums: albums });
   }),
   // Get songs by album ids
   http.get(`${apiUrl}/song/:id`, async ({ params }) => {
     const { id } = params;
     const songs = await songsDB.readSongsByAlbumsIds(id as string[]);
-    return HttpResponse.json(songs);
+    return HttpResponse.json({ songs: songs });
   })
 );
