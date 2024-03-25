de0469e282a5fa52a169d80ee18900833863ac1d
AhmedOsama099 <ahmedeng099@gmail.com>
2024-03-26 01:58:44 +0200

    Add steo4 page structre

--- wizard-app/src/components/CustomWizard/Step4.tsx
+++ wizard-app/src/components/CustomWizard/Step4.tsx
@@ -1,5 +1,30 @@
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import StepWrapper from "./StepWrapper";
+import { TextField } from "@mui/material";
+
 const Step4 = () => {
-  return <div>Step4</div>;
+  return (
+    <>
+      <StepWrapper>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container>
+            <Grid
+              display="flex"
+              flexDirection="column"
+              rowGap={"15px"}
+              justifyContent="center"
+              sm={12}
+            >
+              <TextField id="outlined-basic" label="Name" variant="outlined" />
+              <TextField id="outlined-basic" label="Email" variant="outlined" />
+              <TextField id="outlined-basic" label="Phone" variant="outlined" />
+            </Grid>
+          </Grid>
+        </Box>
+      </StepWrapper>
+    </>
+  );
 };
 
 export default Step4;
